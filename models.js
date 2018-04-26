9baf72fccd7a2f11bb376481292be7dd2c2e3a04
trwiley <taylor.r.wiley@gmail.com>
2018-04-26 13:02:37 -0400

    Added in JS objects associated with the module models

--- models.js
+++ models.js
@@ -1,20 +1,20 @@
 var models = {
     "models":[
         {
-           "name":"Dark Pre-Choctaw Little Bear Creek-Type Projectile Point Arrowhead, 2,500 BC",
-           "description":"flippy flappy",
-           "filepath": "../models/Bear_Creek_Point_Dark.stl"
+           "name":"Australopithecus africanus cranium",
+           "description":"Credits: akkia; Royal Belgian Institute of Natural Sciences (Creative Commons - Attribution - Non-Commercial)",
+           "filepath": "../models/Australopithecus_africanus500kML.stl"
         },
 
         {
-            "name": "Venus of Dolní Věstonice at The Vienna Natural History Museum, Austria",
-            "description": "flippy flappy",
-            "filepath":"../models/venus-of-dolni-vestonice-1.stl"
+            "name": "Homo habilis cranium",
+            "description": "Credits: Darryl Ricketts; Morphosource (Media number: M6228-5965, CC BY-NC-ND)",
+            "filepath":"../models/Homo_habilis_skull_Duke.stl"
         },
         {
-            "name":"Foot, Right Human Fully assembled",
-            "description": "flippy flappy",
-            "filepath":"../models/human-foot-right-assembled.stl"
+            "name":"Homo habilis mandible",
+            "description": "Credits: Darryl Ricketts; Morphosource (Media number: M6309-5959, CC BY-NC-ND)",
+            "filepath":"../models/Homo_habilis_mandible_Duke.stl"
         }
         
     ]
